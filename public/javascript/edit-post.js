fbf67aae07daf64ae9c3614bd7611dee69007ad3
vcaruso <vinnycar0923@gmail.com>
2020-09-03 17:18:48 -0700

    Error handling - choose species/location

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -7,22 +7,25 @@ async function editFormHandler(event) {
     const id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
-
-    const response = await fetch(`/api/posts/${id}`, {
-        method: 'PUT',
-        body: JSON.stringify({
-            type,
-            location
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
-
-    if (response.ok) {
-        document.location.replace('/dashboard');
+    if (type === "" || location === "") {
+        window.alert("Please enter species and location!")
     } else {
-        alert(response.statusText);
+        const response = await fetch(`/api/posts/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify({
+                type,
+                location
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
     }
 }
 
